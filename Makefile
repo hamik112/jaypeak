370fa15a8c86d4388dcfbca559630cbc8863057d
Joshua Massover <joshua.massover@gmail.com>
2016-03-21 23:28:07 -0400

    Add initial recurring transaction admin

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ lint:
 	pep8 config.py jaypeak/yodlee/__init__.py --ignore=E501
 
 test:
-	py.test jaypeak/yodlee/tests.py jaypeak/transactions/tests.py
+	py.test jaypeak/transactions/tests.py jaypeak/yodlee/tests.py
 
 coverage:
 	py.test --cov=jaypeak/yodlee jaypeak/yodlee/tests.py
--- jaypeak/transactions/admin.py
+++ jaypeak/transactions/admin.py
@@ -2,7 +2,7 @@ from flask import redirect, url_for, request
 from flask_admin.contrib.sqla import ModelView
 from flask_security import current_user
 
-from .models import User, Role, Transaction
+from .models import User, Role, Transaction, RecurringTransaction
 from ..extensions import admin
 
 
@@ -46,7 +46,18 @@ class TransactionAdmin(ModelView):
         return redirect(url_for('transaction.login', next=request.url))
 
 
+class RecurringTransactionAdmin(ModelView):
+    column_display_pk = True
+
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('transaction.login', next=request.url))
+
+
 def configure_transactions_admin(app, db):
     admin.add_view(UserAdmin(User, db.session))
     admin.add_view(RoleAdmin(Role, db.session))
     admin.add_view(TransactionAdmin(Transaction, db.session))
+    admin.add_view(RecurringTransactionAdmin(RecurringTransaction, db.session))  # nopep8
